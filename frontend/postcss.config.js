a83c011fe31116f8ace9b31ca6b71d75fdb9e5ea
naphatbass <naphatbass@outlook.com>
2023-04-30 11:10:04 +0700

    fix init
    - use npm instead of yarn

---
+++ frontend/postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: [
+      require('postcss-import'),
+      require('tailwindcss'),
+      require('autoprefixer'),
+      require('postcss-preset-env')({
+        stage: 1,
+      }),
+    ],
+  };
+  
