646600f69df22b84abd8374e5dd8aa5ba8fad288
ThanachatAim <Thanachat.aim@gmail.com>
2023-04-30 22:48:35 +0700

    change chat engine user

--- test-chat-app/frontend/src/ChatsPage.jsx
+++ test-chat-app/frontend/src/ChatsPage.jsx
@@ -1,6 +1,6 @@
 import {MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced'
 const ChatsPage = (props) => {
-    const chatProps = useMultiChatLogic('910f2ace-b809-49e1-9df8-5e72e740e6b9',props.user.username, props.user.secret);
+    const chatProps = useMultiChatLogic('025449c7-e41d-4f36-a330-00b69652ac56',props.user.username, props.user.secret);
     return (
     <div style = {{ height: '100vh'}}>
         <MultiChatSocket {...chatProps} />
