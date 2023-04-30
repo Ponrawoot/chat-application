d77f20653d948c7b15377bf8fe3f95e34be8364f
naphatbass <naphatbass@outlook.com>
2023-04-30 17:57:18 +0700

    template update
    - added template
    TODO:
    - implement prop to component
    - config color theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,42 @@
+const ChatBox=()=>{
+  return(
+    <div className="w-full h-[10%] bg-white border-2 border-gray-800 shrink-0">
+      <p className="text-center">This will be a chat box</p>
+    </div>
+  );
+}
+
 function App() {
   return (
-  <h1 className="text-3xl font-bold underline">
-  Hello world!
-</h1>
+  <div className="flex flex-row w-screen h-screen bg-black">
+    <div className="flex flex-col w-2/5 h-full bg-gray-800">
+      <div className="w-full h-[10%] justify-center">
+        <p className="text-white text-center m-[5%]">This part will be chat list</p>
+      </div>
+    </div>
+
+    <div className="flex flex-col w-3/5 h-full overflow-y-scroll">
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+      <ChatBox/>
+    </div>
+  </div>
+  
   )
 }
 
