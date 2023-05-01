d03954a7fa73e980a1e801ff49636aaa3e2052d8
KenshiroKosuke <mahasamud555@gmail.com>
2023-05-01 22:38:54 +0700

    feat: add all user request

--- test-chat-app/frontend/src/App.jsx
+++ test-chat-app/frontend/src/App.jsx
@@ -4,6 +4,8 @@ import "./App.css";
 
 import AuthPage from "./AuthPage";
 import ChatsPage from "./ChatsPage";
+import { ChatEngine } from "react-chat-engine";
+import DirectChatPage from "./DirectChatPage";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -11,8 +13,25 @@ function App() {
   if (!user) {
     return <AuthPage onAuth={(user) => setUser(user)} />;
   } else {
-    return <ChatsPage user={user} />;
+    console.log(user)
+    return (
+      <DirectChatPage
+        projectID="025449c7-e41d-4f36-a330-00b69652ac56"
+        userName={user.username}
+      />
+    );
   }
 }
 
 export default App;
+
+// import { ChatEngine } from "react-chat-engine";
+// function App() {
+//   return (
+//     <ChatEngine
+//       projectID="025449c7-e41d-4f36-a330-00b69652ac56"
+//       userName="adam"
+//       userSecret="pass1234"
+//     />
+//   );
+// }
