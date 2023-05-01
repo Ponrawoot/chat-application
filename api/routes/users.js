d1cf7ee5487aed3dc65d573cd2a5a6835f43156f
ThanachatAim <Thanachat.aim@gmail.com>
2023-05-01 21:26:49 +0700

    add get all user

--- api/routes/users.js
+++ api/routes/users.js
@@ -55,46 +55,16 @@ router.get("/", async (req, res) => {
   }
 });
 
-//get friends
-router.get("/friends/:userId", async (req, res) => {
+//get all user
+router.get("/all", async (req,res) => {
   try {
-    const user = await User.findById(req.params.userId);
-    const friends = await Promise.all(
-      user.followings.map((friendId) => {
-        return User.findById(friendId);
-      })
-    );
-    let friendList = [];
-    friends.map((friend) => {
-      const { _id, username, profilePicture } = friend;
-      friendList.push({ _id, username, profilePicture });
-    });
-    res.status(200).json(friendList)
+    const user = await User.find({});
+    res.status(200).json(user);
+
   } catch (err) {
     res.status(500).json(err);
   }
-});
-
-//follow a user
-
-router.put("/:id/follow", async (req, res) => {
-  if (req.body.userId !== req.params.id) {
-    try {
-      const user = await User.findById(req.params.id);
-      const currentUser = await User.findById(req.body.userId);
-      if (!user.followers.includes(req.body.userId)) {
-        await user.updateOne({ $push: { followers: req.body.userId } });
-        await currentUser.updateOne({ $push: { followings: req.params.id } });
-        res.status(200).json("user has been followed");
-      } else {
-        res.status(403).json("you allready follow this user");
-      }
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  } else {
-    res.status(403).json("you cant follow yourself");
-  }
+  
 });
 
 //unfollow a user
