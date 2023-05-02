7d860fe70869d13dd29d9d17330c1375352c75a9
naphatbass <naphatbass@outlook.com>
2023-05-02 21:28:06 +0700

    Successful get conversation ID

--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -1,17 +1,16 @@
 import "./messenger.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Chat() {
   const [users, setUsers] = useState([]);
-
   const [user, setUser] = useState("");
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [conversationId, setConversationId] = useState(null);
 
   useEffect(() => {
     const userFromStorage = JSON.parse(localStorage.getItem("user"));
-    //console.log("User ID from storage:", userFromStorage._id);
     setUser(userFromStorage);
   }, []);
 
@@ -27,30 +26,57 @@ export default function Chat() {
     fetchUsers();
   }, []);
 
-
+  const handleUserClick = async (selectedUser) => {
+    setSelectedUser(selectedUser);
+    try {
+      const res = await axios.get(`/conversations/find/${user._id}/${selectedUser._id}`);
+      if (res.data) {
+        setConversationId(res.data._id);
+      } else {
+        setConversationId(null);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="chat">
       <div className="chatMenu">
         <div className="chatMenuWrapper">
           <input placeholder="Search for friends" className="chatMenuInput" />
-          {users.map((u) => (
-            <div className="chatMenuFriend">
-              {/* <img className="chatMenuImg" src={u.profilePicture} alt="" /> */}
-              <span className="chatMenuName">{u.username}</span>
-              {/* <span className="chatMenuId">{u._id}</span> */}
-            </div>
-          ))}
+          {users
+            .filter((u) => u._id !== user._id)
+            .map((u) => (
+              <div
+                className={`chatMenuFriend ${selectedUser === u ? "active" : ""}`}
+                key={u._id}
+                onClick={() => handleUserClick(u)}
+              >
+                <span className="chatMenuName">{u.username}</span>
+              </div>
+            ))}
         </div>
       </div>
-      <div className="chatBox"></div>
-      <div>
-        Username is : {user.username} User ID is : {user._id}
+      <div className="chatBox">
+        {selectedUser && conversationId ? (
+          <div>Conversation ID: {conversationId}</div>
+        ) : (
+          <div>
+            {selectedUser
+              ? `No conversation with ${selectedUser.username} yet`
+              : "Select a user to start a conversation"}
+          </div>
+        )}
       </div>
       <div className="chatOnline">
         <button>
-        <Link to='/groupchat'>New room</Link>
+          <Link to="/groupchat">New room</Link>
         </button>
+        <div>
+          Username is : {user.username} <br />
+          User ID is : {user._id}
+        </div>
       </div>
     </div>
   );
