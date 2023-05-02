b655f8d2d20e56a6440e63c47ab3dc1ed64343e9
naphatbass <naphatbass@outlook.com>
2023-05-02 23:56:30 +0700

    fix login page UI

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -2,8 +2,8 @@ import { useContext, useRef } from "react";
 import "./login.css";
 import { loginCall } from "../../apiCalls";
 import { AuthContext } from "../../context/AuthContext";
-import { CircularProgress } from "@material-ui/core";
-
+import { CircularProgress, Link } from "@material-ui/core";
+import { Link as RouterLink } from 'react-router-dom';
 
 export default function Login() {
   const email = useRef();
@@ -32,12 +32,6 @@ export default function Login() {
   return (
     <div className="login">
       <div className="loginWrapper">
-        <div className="loginLeft">
-          <h3 className="loginLogo">Lamasocial</h3>
-          <span className="loginDesc">
-            Connect with friends and the world around you on Lamasocial.
-          </span>
-        </div>
         <div className="loginRight">
           <form className="loginBox" onSubmit={handleClick}>
             <input
@@ -62,17 +56,21 @@ export default function Login() {
                 "Log In"
               )}
             </button>
-            <span className="loginForgot">Forgot Password?</span>
+            
             <button className="loginRegisterButton">
               {isFetching ? (
                 <CircularProgress color="white" size="20px" />
               ) : (
-                "Create a New Account"
+                <Link component={RouterLink} to="/register" className="link"> {/* Use the RouterLink component to link to /register */}
+                  Create a New Account
+                </Link>
               )}
             </button>
+            
           </form>
         </div>
       </div>
     </div>
   );
 }
+
